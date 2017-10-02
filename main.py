07d164445ed1b8942df822ee0ba262c9a930ae71
Iojiki <eeohjeeki@gmail.com>
2017-10-01 19:57:48 -0500

    got stuff working

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+@app.route("/submit", methods = ['POST'])
+def submit():
+    name = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
+    error = ''
+    if(name == '' or password == '' or verify_password == '' or email == ''):
+        error = "This is a fail"
+        return redirect("/?error=" + error)
+    return render_template('welcome.html', username = name, error = error)
 @app.route("/")
 def index():
-    return render_template('home.html', title = 'index')
+    encoded_error = request.args.get("error")
+
+    return render_template('home.html', title = 'Index', error = encoded_error)
+
+
 
 
 app.run()
